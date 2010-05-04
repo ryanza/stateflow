605b0fadd6699ae4f128329c416c10427d0562ee
Ryan Oberholzer <ryan@platform45.com>
2010-05-04 11:55:27 +0200

    Added support for mongoid

--- lib/stateflow/persistence.rb
+++ lib/stateflow/persistence.rb
@@ -6,10 +6,13 @@ module Stateflow
           Stateflow::Persistence::MongoMapper.install(base)
         when :active_record
           Stateflow::Persistence::ActiveRecord.install(base)
+        when :mongoid
+          Stateflow::Persistence::Mongoid.install(base)
       end
     end
     
     autoload :MongoMapper, 'stateflow/persistence/mongo_mapper'
     autoload :ActiveRecord, 'stateflow/persistence/active_record'
+    autoload :Mongoid, 'stateflow/persistence/mongoid'
   end
 end
