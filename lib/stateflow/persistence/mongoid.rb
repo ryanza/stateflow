2c9524e37c15843438a3bfa8a74a43b7d485c88f
chatgris <chatgri@gmail.com>
2010-05-08 22:18:07 +0800

    Mongoid 2.0.0.beta compliance

--- lib/stateflow/persistence/mongoid.rb
+++ lib/stateflow/persistence/mongoid.rb
@@ -2,10 +2,10 @@ module Stateflow
   module Persistence
     module Mongoid
       def self.install(base)
-        base.respond_to?(:before_validation_on_create) ? base.before_validation_on_create(:ensure_initial_state) : base.before_validation(:ensure_initial_state, :on => :create)
+        base.respond_to?(:before_validate_on_create) ? base.before_validate_on_create(:ensure_initial_state) : base.before_validate(:ensure_initial_state, :on => :create)
         base.send :include, InstanceMethods
       end
-      
+
       module InstanceMethods
         def load_from_persistence
           self.send machine.state_column.to_sym
@@ -14,7 +14,7 @@ module Stateflow
         def save_to_persistence(new_state)
           self.update_attributes(machine.state_column.to_sym => new_state)
         end
-        
+
         def ensure_initial_state
           send("#{self.machine.state_column.to_s}=", self.current_state.name.to_s) if send(self.machine.state_column.to_s).blank?
         end
